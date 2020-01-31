4af9d49e4c8dc2fcc7068a90c09e2ddb325253c6
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-01-31 15:36:14 +0500

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -1,1 +1,9 @@
 
+ [{
+ 	"url": "https://www.google.com.pk/",
+ 	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
+ },{
+ 	"url": "https://www.google.com.pk/",
+ 	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
+ }
+ ]

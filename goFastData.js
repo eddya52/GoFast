cee81575b4e6f68ae7996434279a8c070e9f60f2
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-02-01 20:36:37 +0500

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -1,9 +1,10 @@
 
  [{
+ 	"isRemote": "true",
  	"url": "https://www.google.com",
  	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
- },{
+ }, {
+ 	"isRemote": "true",
  	"url": "https://toggl.com/app/3843219/clients",
  	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
- }
- ]
+ }]

ecba0363bb9aa39efa6deed424fb3671a69371f1
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-02-01 18:14:02 +0500

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -1,6 +1,6 @@
 
  [{
- 	"url": "https://www.google.com.pk/",
+ 	"url": "https://www.google.com",
  	"requirments": ["Enteracombination of atleast six numbers", "letters and punctuation marks (suchas!and&)"]
  },{
  	"url": "https://toggl.com/app/3843219/clients",

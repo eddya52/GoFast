cc273c6f3151f5aec5672b0f24513ae0d0033dd9
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-02-14 23:10:56 -0800

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -2,9 +2,13 @@
  [{
  	"isRemote": "true",
  	"url": "https://www.google.com",
- 	"requirments": ["Enter a combination of at least six numbers", "letters and punctuation marks (such as ! and &)"]
+ 	"requirments": ["Enter a combination of at least six characters", "letters and punctuation marks (such as ! and &)"]
  }, {
  	"isRemote": "true",
  	"url": "https://www.facebook.com/",
- 	"requirments": ["Enter a combination of at least six numbers", "letters and punctuation marks (such as ! and &)"]
+ 	"requirments": ["Enter a combination of at least six characters", "letters and punctuation marks (such as ! and &)"]
+ }, {
+ 	"isRemote": "true",
+ 	"url": "https://twitter.com/",
+ 	"requirments": ["Enter a combination of at least ten characters", "include lowercase letters", "include upper case letters", "include numbers and symbols (such as ! and ?"]
  }]

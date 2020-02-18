ae4298326a84666e38da3d06c2e2c0166690ddea
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-02-17 21:52:46 -0800

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -33,39 +33,39 @@
  	"requirments": ["Enter a combination of at least six (6) characters"]
  }, {
  	"isRemote": "true",
- 	"url": "https://twitter.com/",
+ 	"url": "https://www.twitter.com/",
  	"requirments": ["Enter a combination of at least ten (10) characters", "Include lowercase letters", "include upper case letters", "Include numbers and symbols (such as ! and ?)"]
  }, {
  	"isRemote": "true",
- 	"url": "https://linkedin.com/",
+ 	"url": "https://www.linkedin.com/",
  	"requirments": ["Enter a combination of at least six (6) characters"]
  }, {
  	"isRemote": "true",
- 	"url": "https://hulu.com/",
+ 	"url": "https://www.hulu.com/",
  	"requirments": ["Enter a combination of at least six (6) characters"]
  }, {
  	"isRemote": "true",
- 	"url": "https://twitch.tv/",
+ 	"url": "https://www.twitch.tv/",
  	"requirments": ["Enter a combination of at least eight (8) characters"]
  }, {
  	"isRemote": "true",
- 	"url": "https://netflix.com/",
+ 	"url": "https://www.netflix.com/",
  	"requirments": ["Enter a combination of at least four (4) characters", "Cannot exceed sixty (60) characters in length"]
  }, {
  	"isRemote": "true",
- 	"url": "https://amazon.com/",
+ 	"url": "https://www.amazon.com/",
  	"requirments": ["Enter a combination of at least six (6) characters", "Must not be a previously used password"]
  }, {
  	"isRemote": "true",
- 	"url": "https://amazon.ca/",
+ 	"url": "https://www.amazon.ca/",
  	"requirments": ["Enter a combination of at least six (6) characters", "Must not be a previously used password"]
  }, {
  	"isRemote": "true",
- 	"url": "https://lastpass.com/",
+ 	"url": "https://www.lastpass.com/",
  	"requirments": ["Enter a combination of at least twelve (12) characters", "Must use upper case letters, lower case letters, numbers, and special characters"]
  }, {
  	"isRemote": "true",
- 	"url": "https://paypal.com/",
+ 	"url": "https://www.paypal.com/",
  	"requirments": ["Enter a combination of at least seven (7) characters", "Cannot exceed thirty two (32) characters in length", "Include letters, numbers, and special characters (such as ! and &)", "Password cannot be the same as your login", "Single quotes, double quotes, ampersands, and spaces are not allowed", "Must not have only letters or only numbers" ]
  }, {
  	"isRemote": "true",

3067a0a00d7ac14f3d168713459d04041ea93e87
eddya52 <60458296+eddya52@users.noreply.github.com>
2020-02-17 21:25:17 -0800

    Update goFastData.js

--- goFastData.js
+++ goFastData.js
@@ -2,13 +2,17 @@
  [{
  	"isRemote": "true",
  	"url": "https://www.google.com",
- 	"requirments": ["Enter a combination of at least six characters", "letters and punctuation marks (such as ! and &)"]
+ 	"requirments": ["Enter a combination of at least eight (8) characters", "Include a number", "Include a capital letter", "Include a lower case letter", "Password cannot match username"]
+ },{
+ 	"isRemote": "true",
+ 	"url": "https://www.google.ca",
+ 	"requirments": ["Enter a combination of at least eight (8) characters", "Include a number", "Include a capital letter", "Include a lower case letter", "Password cannot match username"]
  }, {
  	"isRemote": "true",
  	"url": "https://www.facebook.com/",
- 	"requirments": ["Enter a combination of at least six characters", "letters and punctuation marks (such as ! and &)"]
+ 	"requirments": ["Enter a combination of at least six (6) characters", "Include letters and punctuation marks (such as ! and &)"]
  }, {
  	"isRemote": "true",
  	"url": "https://twitter.com/",
- 	"requirments": ["Enter a combination of at least ten characters", "include lowercase letters", "include upper case letters", "include numbers and symbols (such as ! and ?"]
+ 	"requirments": ["Enter a combination of at least ten (10) characters", "Include lowercase letters", "include upper case letters", "Include numbers and symbols (such as ! and ?)"]
  }]
